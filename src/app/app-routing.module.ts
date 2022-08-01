907699a1caf0d338f526c37f65760b4178113d2c
MuhammadSameh <muhammadsameh970@gmail.com>
2022-08-01 19:38:13 +0200

    CRUD

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddProductComponent } from 'src/Components/add-Product/add-Product.component';
+import { AddVariationComponent } from 'src/Components/add-variation/add-variation.component';
+import { HomeComponent } from 'src/Components/home/home.component';
+import { LoginComponent } from 'src/Components/login/login.component';
+import { RegisterComponent } from 'src/Components/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'Register', component:RegisterComponent},
+  {path:'Login', component:LoginComponent},
+  {path:'Add', component:AddProductComponent},
+  {path:'Home/:sid', component:HomeComponent},
+  {path:'Variation/:pid', component:AddVariationComponent},
+  {path:'', component:LoginComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,51 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from 'src/Components/register/register.component';
+import { LoginComponent } from 'src/Components/login/login.component';
+import { AddProductComponent } from 'src/Components/add-Product/add-Product.component';
+import { HomeComponent } from 'src/Components/home/home.component';
+import {HttpClientModule} from '@angular/common/http'
+import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from 'src/Components/header/header.component';
+import { ProductComponent } from 'src/Components/product/product.component';
+import { AddVariationComponent } from 'src/Components/add-variation/add-variation.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import {MatDialogModule, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { DeleteConfirmationDialogComponent } from 'src/Components/delete-confirmation-dialog/delete-confirmation-dialog.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    HomeComponent,
+    AddProductComponent,
+    HeaderComponent,
+    ProductComponent,
+    AddVariationComponent,
+    DeleteConfirmationDialogComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule
   ],
-  providers: [],
+  providers: [  
+    { provide: MAT_DIALOG_DATA, useValue: {} },
+    { provide: MatDialogRef, useValue: {} }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
